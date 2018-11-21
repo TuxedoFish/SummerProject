3b839d6ce88b50ce3f3dd44b83e71dbb1098a2b9
TuxedoFish <harryliversedge@gmail.com>
2018-11-21 21:30:16 +0000

    updated the way the user checks if the size of the group has changed

--- app/src/main/java/com/example/harry/umbrellafindr/app/DecisionActivity.java
+++ app/src/main/java/com/example/harry/umbrellafindr/app/DecisionActivity.java
@@ -1,5 +1,6 @@
 package com.example.harry.umbrellafindr.app;
 
+import android.graphics.Paint;
 import android.graphics.drawable.RippleDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -44,6 +45,7 @@ public class DecisionActivity extends AppCompatActivity implements View.OnTouchL
             @Override
             public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
                 nameText.setText((String)documentSnapshot.get("first_name"));
+                nameText.setPaintFlags(nameText.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
             }
         });
 
